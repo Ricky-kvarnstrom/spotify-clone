cd582651a06f7651e4b049aa331147b3b82cfcd4
Ricky K <riickyy_kvarn@hotmail.se>
2023-03-27 11:42:41 +0200

    added login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { accessUrl } from "../config";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="'py-10 text-center">
+      <h1 className="text-4xl font-bold mb-1">Welcome to my spotify clone</h1>
+      <p className="text-text-dimmed mb-10">Please sign in use the app</p>
+      <a
+        href={accessUrl}
+        className="py-1.5 px-5 bg-primary rounded-md text-xl font-semibold"
+      >
+        Sign in
+      </a>
+    </div>
+  );
 }
